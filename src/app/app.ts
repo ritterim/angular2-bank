4801ea64c7f99eb8f28a64076fa4eec967794743
Ken Dale <ken@kendaleiv.com>
2015-12-15 08:50:52 -0500

    Minor cleanup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES, FORM_PROVIDERS} from 'angular2/common';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES } from 'angular2/common';
 
 import {ShowBalancesComponent} from '../components/ShowBalancesComponent';
 import {Bank} from '../bank';
@@ -7,7 +7,6 @@ import {Bank} from '../bank';
 @Component({
   directives: [ CORE_DIRECTIVES, FORM_DIRECTIVES, ShowBalancesComponent ],
   pipes: [],
-  // providers: [ FORM_PROVIDERS, Bank ],
   providers: [ Bank ],
   selector: 'app',
   styles: [ require('./app.css') ],
