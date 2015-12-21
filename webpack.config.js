08618a8f25b6f94bf485ce6e594ef6c1ee1742e3
Ken Dale <ken@kendaleiv.com>
2015-12-21 13:06:12 -0500

    Update to https://github.com/AngularClass/angular2-webpack-starter/commit/a36735ed3f4ee358a93db13e3d8f1a45b765694f

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@
 /*
  * Helper: root(), and rootDir() are defined at the bottom
  */
-var sliceArgs = Function.prototype.call.bind(Array.prototype.slice);
-var toString  = Function.prototype.call.bind(Object.prototype.toString);
 var path = require('path');
 var webpack = require('webpack');
 // Webpack Plugins
@@ -16,7 +14,7 @@ var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 module.exports = {
   // for faster builds use 'eval'
   devtool: 'source-map',
-  debug: true,
+  debug: true, // remove in production
 
   entry: {
     'vendor': './src/vendor.ts',
@@ -46,12 +44,12 @@ module.exports = {
         query: {
           'ignoreDiagnostics': [
             2403, // 2403 -> Subsequent variable declarations
-            2300, // 2300 Duplicate identifier
+            2300, // 2300 -> Duplicate identifier
             2374, // 2374 -> Duplicate number index signature
             2375  // 2375 -> Duplicate string index signature
           ]
         },
-        exclude: [ /\.spec\.ts$/, /\.e2e\.ts$/, /node_modules/ ]
+        exclude: [ /\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/ ]
       },
 
       // Support for *.json files.
@@ -61,13 +59,15 @@ module.exports = {
       { test: /\.css$/,   loader: 'raw-loader' },
 
       // support for .html as raw text
-      { test: /\.html$/,  loader: 'raw-loader' }
-    ]
+      { test: /\.html$/,  loader: 'raw-loader' },
+    ],
+    noParse: [ /.+zone\.js\/dist\/.+/, /.+angular2\/bundles\/.+/ ]
   },
 
   plugins: [
     new CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js', minChunks: Infinity }),
-    new CommonsChunkPlugin({ name: 'common', filename: 'common.js', minChunks: 2, chunks: ['app'] })
+    new CommonsChunkPlugin({ name: 'common', filename: 'common.js', minChunks: 2, chunks: ['app', 'vendor'] })
+    // include uglify in production
   ],
 
   // Other module loader config
@@ -81,17 +81,16 @@ module.exports = {
     contentBase: 'src/public',
     publicPath: '/__build__'
   }
-
 };
 
 // Helper functions
 
 function root(args) {
-  args = sliceArgs(arguments, 0);
+  args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [__dirname].concat(args));
 }
 
 function rootNode(args) {
-  args = sliceArgs(arguments, 0);
+  args = Array.prototype.slice.call(arguments, 0);
   return root.apply(path, ['node_modules'].concat(args));
 }
